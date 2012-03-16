0d1336b2d684067016211b208ed39efca8e5f8b3
Bruno Alvares da Costa <brunoadacosta@gmail.com>
2012-03-15 22:17:31 -0300

    arrumando load no heroku
    mend

--- config/boot.rb
+++ config/boot.rb
@@ -4,12 +4,13 @@ ENV["RACK_ENV"] = "development"
 Bundler.setup(:default, ENV["RACK_ENV"])
 Bundler.require
 
+Dir[File.expand_path("../../config/initializers/**/*.rb", __FILE__)].sort.each do |file|
+  require file
+end
+
 Dir[File.expand_path("../../app/**/*.rb", __FILE__)].sort.each do |file|
   require file
 end
 
-Dir[File.expand_path("../../config/initializers/**/*.rb", __FILE__)].sort.each do |file|
-  require file
-end
 
 helpers ApplicationHelper
