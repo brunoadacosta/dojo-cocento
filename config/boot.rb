8be37672c9e2707f4a747ca6a45c6df1c15bc7e3
Bruno Alvares da Costa <brunoadacosta@gmail.com>
2012-03-15 22:13:10 -0300

    arrumando load dos arquivos no heroku

--- config/boot.rb
+++ config/boot.rb
@@ -4,7 +4,7 @@ ENV["RACK_ENV"] = "development"
 Bundler.setup(:default, ENV["RACK_ENV"])
 Bundler.require
 
-Dir[File.expand_path("../../app/**/*.rb", __FILE__)].each do |file|
+Dir[File.expand_path("../../app/**/*.rb", __FILE__)].sort.each do |file|
   require file
 end
 
